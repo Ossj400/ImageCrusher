bf8db0150f74181b0bc7741c72bb792b510e9788
Artur Osmanowski <ossj400@gmail.com>
2020-02-06 02:01:18 +0100

    Cleaned algo

--- Inpainting/Nans.cs
+++ Inpainting/Nans.cs
@@ -27,8 +27,6 @@ namespace ImageCrusher.Inpainting
         {
             int i = 0;
             int j = 0;
-            int ij = 0;
-            int l = 0;
             
             var A = imageIn;
             var mas = mask;
@@ -38,30 +36,18 @@ namespace ImageCrusher.Inpainting
             int nmc = n*m*c;
 
             int[,] nan_rc = new int[mas.Height, mas.Width + 1];
-            int[] k = new int[nmc]; // finding NaNs in one dim. array and making it 1 if finds it. Any value in mask is failure so mask values can be 'k' values but converted in 1 dim arr
-            foreach (int item in mas.Data)  // convert to 1 dim and set where are Nans in rows and cols.
+            int[] k = new int[nmc]; //Any value in mask is "failure"/NaN so mask values can be 'k' values but converted in 1 dim arr
+            foreach (int item in mas.Data)  // convert to 1 dim and sets where are Nans in rows and cols.
             {
-                //if (ij == mas.Height-1)
-                //    ij = 0;
-                //if (j == mas.Width)
-                //    j = 0;
-
-                //if (mas.Data[ij,j,0]>0)
-                //{
-                //    nan_rc[ij, j] = j;
-                //}
                 k[i] = item;
                 i++;
-                //j++;
-                //ij++;
             }
             i = 0;
-            j = 0;
 
-            string nan_list_int = mas.CountNonzero().GetValue(0).ToString();
-            Int32.TryParse(nan_list_int, out int nan_list_1);
+            string nan_list_string = mas.CountNonzero().GetValue(0).ToString();
+            Int32.TryParse(nan_list_string, out int nan_list_1);   // nan_list_1 is int value of NaNs
 
-            int[] nan_list_2 = new int[nan_list_1];
+            int[] nan_list_2 = new int[nan_list_1];  // clean list of NaN pixels
             int[] known_list = new int[nmc-nan_list_1];
             foreach(int item in k)
             {
@@ -72,9 +58,7 @@ namespace ImageCrusher.Inpainting
                 }
                 i++;
             }
-            i = 0;
-            j = 0;
-
+            i = 0; j = 0;
             foreach (int item in k)
             {
                 if (item == 0)
@@ -84,13 +68,11 @@ namespace ImageCrusher.Inpainting
                 }
                 i++;
             }
-            i = 0;
-            j = 0;
-            ij = 0;
+            i = 0;j = 0;
 
-            int nan_count = nan_list_2.Length;
+            int nan_count = nan_list_1;
 
-            int[,] nan_list_3 = new int[nan_count+1,3]; 
+            int[,] nan_list = new int[nan_count+1,3]; 
             int col = 0;
             int row = 0;
             foreach (int item in k)
@@ -106,51 +88,24 @@ namespace ImageCrusher.Inpainting
                 }
               if(item != 0)
                 {
-                    nan_list_3[j, 0] = item;// nan_list_2[j];     // dobrze ?
+                    nan_list[j, 0] = item;    // dobrze ?
                     j++;
                     {
-                        nan_list_3[j, 1] = row;
+                        nan_list[j, 1] = row;
                     }
                     {
-                        nan_list_3[j, 2] = col;
+                        nan_list[j, 2] = col;
                     }
                 }
                i++;
                col++;
             }
-            i = 0;
-            j = 0;
-            row = 0;
-            col = 0;
+            i = 0;j = 0;row = 0;col = 0;
 
-            //int[,,] nan_list_4 = new int[nan_count, 2 , 2];
-            //foreach(int item in nan_list_3)
-            //{
-            //    if (mas.Height == col)
-            //    {
-            //        col = 0;
-            //    }
-            //    if (i > 3)
-            //    {
-            //        if ((i + 1) % mas.Width == 1)
-            //            row++;
-            //    }
-            //    if (item != 0)
-            //    {
-            //        nan_list_4[i,0,0] = item;      //val                      
-            //        {
-            //            nan_list_4[0, i, 0] = row;
-            //        }
-
-
-            //        i++;
-            //    }
-            //    col++;
-            //}
 
             int[,] talks_to = new int[4,2] { { -1, 0 },{ 0, -1 }, { -1, 1 }, { 0, 1 } };  // dobrze ?
 
-            IdentifyNeighbours(n, m, nan_list_3, talks_to);
+            IdentifyNeighbours(n, m, nan_list, talks_to);
         }
 
         public void IdentifyNeighbours(int n, int m, int[,] nan_list, int[,] talks_to)

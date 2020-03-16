8600758294182be153d0ad705d2b5d1db285f003
Artur Osmanowski <ossj400@gmail.com>
2020-03-16 20:56:47 +0100

    Optimizing NaNs

--- Inpainting/Nans.cs
+++ Inpainting/Nans.cs
@@ -184,21 +184,47 @@ namespace ImageCrusher.Inpainting
             }
             row = 0; col = 0;
 
-            for (row = 0; row < fda.innerobj.tablesize; row++)
-            {      
-                if (alglib.sparseget(fda, row, col) == -2 && alglib.sparseget(M, row, col) == -2 && row > n && row < alglib.sparsegetnrows(fda)- n)
-                {
-                    alglib.sparseset(fda,(row), col, (alglib.sparseget(M, row, col) + alglib.sparseget(fda, row, col)));
-                }
-                if (row == alglib.sparsegetnrows(fda) - 1)
-                {
-                    row = 0; col++;
-                    if (col == alglib.sparsegetncols(fda))
+            //for (row = 0; row < fda.innerobj.tablesize; row++)
+            //{      
+            //    if (alglib.sparseget(fda, row, col) == -2 && alglib.sparseget(M, row, col) == -2 && row > n && row < alglib.sparsegetnrows(fda)- n)
+            //    {
+            //        alglib.sparseset(fda,(row), col, (alglib.sparseget(M, row, col) + alglib.sparseget(fda, row, col)));
+            //    }
+            //    if (row == alglib.sparsegetnrows(fda) - 1)
+            //    {
+            //        row = 0; col++;
+            //        if (col == alglib.sparsegetncols(fda))
 
-                        break;
+            //            break;
+            //    }
+            //}
+            //i = 0;
+
+            row = 0; col = 0;
+
+            int chuj1 = 0;
+            int chujwazne2 = 0;
+            row = 0;
+            col = 0;
+            double numb;
+            alglib.sparsematrix fda2;
+            alglib.sparsecopy(fda, out fda2);
+            for (i = 0; i < 100000; i++)
+            {
+               if(alglib.sparseenumerate(fda, ref chuj1, ref chujwazne2, out row, out col, out numb) == true)
+                {
+                    if (alglib.sparseget(fda, row, col) == -2 && alglib.sparseget(M, row, col) == -2 && row > n && row < alglib.sparsegetnrows(fda) - n)
+                    {
+                        alglib.sparseset(fda2, (row), col, (alglib.sparseget(M, row, col) + alglib.sparseget(fda, row, col)));
+                        ij++;
+                    }
+                }
+                else
+                {
+                    break;
                 }
             }
-            i = 0;
+            ij = 0;
 
             alglib.sparsematrix rhsSparse;
             alglib.sparsecreate(m * n, knownList.Length, out rhsSparse);
@@ -341,8 +367,17 @@ namespace ImageCrusher.Inpainting
             alglib.linlsqrsolvesparse(linlsqrstate, solvingInputA1, solvingInputB, _params: _params);
             alglib.linlsqrreport report = new alglib.linlsqrreport();
             alglib.linlsqrresults(linlsqrstate, out solve, out report);
-            //   alglib.sparseenumerate()
-            linlsqrstate.Dispose();
+            //int starrt = 0;
+            //int end = 100;
+            //int starrt1 =11;
+            //int end2 = 222;
+            //double numb;
+            //for (i = 0; i < 100; i++)
+            //{
+            //    alglib.sparseenumerate(fda, ref starrt, ref end, out starrt1, out end2, out numb);
+            //    solve[i] = numb;
+            //}
+            //linlsqrstate.Dispose();
 
             i = 0;
             foreach(double item in solve)
